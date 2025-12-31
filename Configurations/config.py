549597b666a649f6bf546b0eb1e0ab25375e2caf
Daanesh Bogale <156383131+DaaneshB@users.noreply.github.com>
2025-12-31 22:00:00 +0000

    comment changes

--- Configurations/config.py
+++ Configurations/config.py
@@ -3,11 +3,59 @@ INTERFACE = #
 L0G_FILE = #
 BLOCKED_IPS = set()
 
-# format of rules: {"name": "Type of Attack", "pattern": "text", "ports": "[src, dest]"}
+# format of rules for signature based detection: {"name": "Type of Attack", "pattern": "text", "ports": "[X, Y]"}
 RULES = [
+    # --- HIGH FIDELITY ATTACKS ---
     {
-        "name": "SQL Injection",
-        "pattern": "UNION SELECT",
+        "name": "SQL Injection (Union Based)", 
+        "pattern": "UNION SELECT", 
+        "ports": [80, 8080, 443]
+    },
+    {
+        "name": "SQL Injection (Boolean Blind)", 
+        "pattern": "' OR '1'='1", 
+        "ports": [80, 8080, 443]
+    },
+    {
+        "name": "Cross-Site Scripting (XSS)", 
+        "pattern": "<script>", 
+        "ports": [80, 8080, 443]
+    },
+    {
+        "name": "Directory Traversal (Linux)", 
+        "pattern": "/etc/shadow", 
+        "ports": [80, 8080, 443]
+    },
+    {
+        "name": "Directory Traversal (Windows)", 
+        "pattern": "\\windows\\system32", 
+        "ports": [80, 8080, 443]
+    },
+
+    {
+        "name": "Automated SQL Scanner (sqlmap)", 
+        "pattern": "sqlmap", 
         "ports": [80, 8080]
     },
+    {
+        "name": "Nikto Web Scanner", 
+        "pattern": "Nikto", 
+        "ports": [80, 8080]
+    },
+
+    {
+        "name": "PHP Reverse Shell", 
+        "pattern": "c99.php", 
+        "ports": [80, 8080]
+    },
+    {
+        "name": "Remote Code Execution (Log4j)", 
+        "pattern": "${jndi:", 
+        "ports": [80, 8080, 8443]
+    },
+    {
+        "name": "Private Key Theft", 
+        "pattern": "-----BEGIN RSA PRIVATE KEY-----", 
+        "ports": [80, 8080, 21, 22]
+    }
 ]
